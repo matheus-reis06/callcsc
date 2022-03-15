2d719c02982f7a9b329e53445ba5970a5aeca236
Math <m.reissilva@protonmail.com>
2022-03-15 16:54:07 -0300

    first commit

---
+++ backend/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+require('dotenv').config();
+const postRouter = require('./routers/post');
+
+const app = express()
+app.use('/api/post', postRouter)
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+    console.log('port is listining on ' + PORT)
+})
